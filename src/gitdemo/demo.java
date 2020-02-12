3d019410bfdc5c53be02ea82c55db1fb9247c837
swetha <pswetha.bi>
2020-02-12 13:14:40 -0500

    Second  commit complete

--- src/gitdemo/demo.java
+++ src/gitdemo/demo.java
@@ -13,11 +13,16 @@ public class demo {
 		int n1=0,n2=1,n3,i,count=10;    
 		 //System.out.print(n1+" "+n2);//printing 0 and 1    
 		 logger.warn("printing  output is "   +n1+" "+n2);
-		    
+		 
 		 for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
 		 {    
 		  n3=n1+n2;    
-		  System.out.print(" "+n3);  
+		  System.out.print(" "+n3);
+		  logger.debug("Febonacci series is ");
+		  logger.warn("Febonacci series is ");
+		  logger.info("Febonacci series is ");
+		  logger.fatal("Febonacci series is ");
+		  logger.error("Febonacci series is ");
 		logger.warn(" "+n3);
 		  n1=n2;    
 		  n2=n3;    
